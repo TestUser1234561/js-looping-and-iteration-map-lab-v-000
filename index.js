443f1105c06d09efdfee17bdefd3fe9f7463d62c
Morgan Fudge <mfudge92@gmail.com>
2018-03-02 02:02:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function lowerCaseDrivers(arr) {
 function nameToAttributes(arr) {
   let test = arr.map((driver) => {
     return {
-      'firstName' : driver.split(' ')
+      'firstName' : driver.split(' ')[0]
     }
   })
   console.log(test)
